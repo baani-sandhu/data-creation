731ac07303187d9695dad666eb6457b249fdb054
nervesparksdev08 <nervesparksdev08@gmail.com>
2026-01-15 17:43:19 +0530

    deleted old folders

--- day02/server/app/server.ts
+++
@@ -1,8 +0,0 @@
-import app from './app.ts';
-import dotenv from 'dotenv';
-dotenv.config();
-const PORT = process.env.PORT
-
-app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
-});
